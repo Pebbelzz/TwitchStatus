d073c4e3b7dce6ff9d8374af49756933ca0f21c9
Pebbelzz <greeneyes14j@gmail.com>
2016-04-30 15:07:42 -0400

    Changing styling

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var streamers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff", "cretetion","TR7K","OgamingSC2","ESL_SC2"];
+var streamers = ["freecodecamp","RobotCaleb","noobs2ninjas", "elvinemod", "veasna94","beohoff", "cretetion","TR7K","OgamingSC2","ESL_SC2"];
+
+
 
 function getChannelInfo() {
   streamers.forEach(function(channel) {
@@ -19,34 +21,23 @@ function getChannelInfo() {
       };
 
       $.getJSON(createURL("channels", channel), function(data) {
-
         var logo = data.logo != null ? data.logo : "http://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F",
           name = data.display_name != null ? data.display_name : channel,
           description = status === "online" ? ': ' + data.status : "";
-          html = '<div class="row ' + status + '"><div id="icon"><img src="' +
-          logo + '" id="pic"></div><div id="name"><a href="' + data.url + '" target="_blank">' +
-          name + '</a></div><div id="streaming">'+ streaming + '<span>' +
-          description + '</span></div></div>';
+          html = '<div class="row channel ' + status + ' ' + streaming + ' ">' +
+          '<div id="icon"><a href="' + data.url + '" target="_blank"><img src="' + logo + '" id="pic" /></a></div>' +
+          '<div id="infoWrapper">' +
+          '<div id="name"><a href="' + data.url + '" target="_blank">'  + name + '</a></div>' +
+          '<div id="streaming"><span id="' + streaming + 'text">'+ streaming + '</span><span>' + description + '</span></div>' +
+          '</div>' +
+          '</div>';
         status === "online" ? $("#display").prepend(html) : $("#display").append(html);
       });
     });
   });
 };
 
+
 $(document).ready(function() {
   getChannelInfo();
-  $(".selector").click(function() {
-    $(".selector").removeClass("active");
-    $(this).addClass("active");
-    var status = $(this).attr('id');
-    if (status === "all") {
-      $(".online, .offline").removeClass("hidden");
-    } else if (status === "online") {
-      $(".online").removeClass("hidden");
-      $(".offline").addClass("hidden");
-    } else {
-      $(".offline").removeClass("hidden");
-      $(".online").addClass("hidden");
-    }
-  })
 });
